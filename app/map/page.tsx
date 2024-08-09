e0bf7cb10116af0f4358846d630d9014476c74a4
Artem Honcharenko <artemgonchar7@gmail.com>
2024-08-09 20:25:27 +0200

    feat: implemented Map page with its components & constants

---
+++ app/map/page.tsx
@@ -0,0 +1,11 @@
+import { MapTemplate } from '@/components/map-template'
+
+const Map = () => (
+  <main className="flex flex-col items-center p-5">
+    <h2 className="text-h2">Map</h2>
+
+    <MapTemplate />
+  </main>
+)
+
+export default Map
