1836a0be30af4bf4b72a6ebca998c1f88296de09
Artem Honcharenko <artemgonchar7@gmail.com>
2024-08-09 19:09:41 +0200

    feat: added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import ButtonLink from '@/components/button-link'
+
+const NotFound = () => (
+  <main className="min-h-full flex flex-col items-center my-20 gap-y-5">
+    {/*Text info block*/}
+    <div className="flex flex-col items-center">
+      <Image priority src="/not-found.svg" width={150} height={150} alt="404" />
+      <h2 className="text-h2 font-bold text-secondary-error">Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+
+    <ButtonLink href="/" text="Return Home" />
+  </main>
+)
+
+export default NotFound
