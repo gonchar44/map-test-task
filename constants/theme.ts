173d4548d70b1acaee8e90cdee7772c83b6276ac
Artem Honcharenko <artemgonchar7@gmail.com>
2024-08-09 13:35:33 +0200

    initial commit

---
+++ constants/theme.ts
@@ -0,0 +1,14 @@
+export const themeColors = {
+  primary: {
+    dark: '#2a2a2a',
+    light: '#ffffff',
+    muted: '#dccaab'
+  },
+  secondary: {
+    dark: 'rgba(0, 0, 0, 0.5)',
+    light: '#a1a1a1',
+    error: '#ef3939',
+    success: '#478f47',
+    transparent: 'transparent'
+  }
+}
