b57c769e260c2079570f51c7df6b92676e430c89
Artem Honcharenko <artemgonchar7@gmail.com>
2024-08-09 20:53:05 +0200

    feat: changed metadata for main layout & Map page

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,8 +1,14 @@
 import { MapTemplate } from '@/components/map-template'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Find your address',
+  description: 'Find your address quickly and conveniently'
+}
 
 const Map = () => (
   <main className="flex flex-col items-center p-5">
-    <h2 className="text-h2">Map</h2>
+    <h3 className="text-h3">Find your address</h3>
 
     <MapTemplate />
   </main>
