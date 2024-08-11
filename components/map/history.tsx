dc51587912557f3f63541b5a57383e07495eb6ef
Artem Honcharenko <artemgonchar7@gmail.com>
2024-08-11 18:11:46 +0200

    feat: implemented logic of history deleting with its confirmation process

--- components/map/history.tsx
+++ components/map/history.tsx
@@ -1,33 +1,75 @@
 'use client'
 
-import { ErrorMessage, Loader } from '@/components'
+import {
+  DeleteButton,
+  DeletingConfirmation,
+  ErrorMessage,
+  Loader
+} from '@/components'
 import { useMapContext } from '@/contexts'
+import { useState } from 'react'
 
 export const History = () => {
-  const { isHistoryLoading, historyPlaces, historyError } = useMapContext()
+  const [deletingItemId, setDeletingItemId] = useState<string | null>(null)
+  const [isDeletingLoading, setIsDeletingLoading] = useState(false)
+  const { isHistoryLoading, historyPlaces, historyError, fetchHistoryData } =
+    useMapContext()
+
+  const deleteById = async (id: string) => {
+    setIsDeletingLoading(true)
+    try {
+      const response = await fetch(`/api/places/${id}`, {
+        method: 'DELETE'
+      })
+      if (!response.ok) {
+        const errorData = await response.json()
+        // TODO: solve throw issue
+        throw new Error(errorData.error)
+      }
+      const result = await response.json()
+
+      if (result) {
+        fetchHistoryData().then()
+      }
+    } finally {
+      setIsDeletingLoading(false)
+    }
+  }
 
   return (
     <div className="w-72">
       <h4 className="text-h4">Search History</h4>
 
-      {isHistoryLoading && <Loader />}
+      {isHistoryLoading && !historyPlaces.length ? (
+        <Loader />
+      ) : (
+        <>
+          {historyPlaces && (
+            <ul className="flex flex-col gap-y-2 py-2">
+              {historyPlaces.map((place) => (
+                <li className="flex items-center gap-x-1" key={place.id}>
+                  <span className="w-10/12 bg-primary-muted rounded text-primary-light p-2 block truncate">
+                    {place.formatted_address}_{place.formatted_address}
+                  </span>
 
-      {historyError && <ErrorMessage text={historyError} />}
+                  {deletingItemId === place.id ? (
+                    <DeletingConfirmation
+                      isDisabled={isDeletingLoading}
+                      onConfirm={() => deleteById(place.id)}
+                      onCancel={() => setDeletingItemId(null)}
+                    />
+                  ) : (
+                    <DeleteButton
+                      onDelete={() => setDeletingItemId(place.id)}
+                    />
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
 
-      {historyPlaces && (
-        <ul className="flex flex-col gap-y-2 py-2">
-          {historyPlaces.map((place) => (
-            <li
-              className="flex justify-between items-center bg-primary-muted rounded text-primary-light p-2"
-              key={place.id}
-            >
-              <span className="block truncate">
-                {place.formatted_address}_{place.formatted_address}
-              </span>
-              <button>Del</button>
-            </li>
-          ))}
-        </ul>
+          {historyError && <ErrorMessage text={historyError} />}
+        </>
       )}
     </div>
   )
